7f0d8ef0d9fbd6049baf057d80237c31cc3bf36e
Tristan Danino <tristandanino@gmail.com>
2019-11-05 21:22:18 +1100

    Added MapForItinerary class

---
+++ GitUseExample/MapForItinerary.swift
@@ -0,0 +1,13 @@
+//
+//  MapForItinerary.swift
+//  GitUseExample
+//
+//  Created by Tristan Danino on 5/11/19.
+//  Copyright © 2019 Tristan Danino. All rights reserved.
+//
+
+import UIKit
+
+class MapForItinerary: NSObject {
+
+}
